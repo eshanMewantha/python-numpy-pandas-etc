72ca42b8f006d7162bb9e34655e99196de2f8233
Eshan Mewantha Herath <eshanMewantha@users.noreply.github.com>
2017-12-26 22:07:42 +0530

    camel case to normal string

--- python/basics/regex.py
+++ python/basics/regex.py
@@ -1,4 +1,15 @@
 import re
+
+print('\n01. Replacing special characters')
 sentence = "I got to meet Elon Musk !!!! And also got to visit @SpaceX. It was F**king Awesome"
+print('Original  : ' + sentence)
 special_characters_replaces = re.sub('[^a-zA-Z0-9 \n\.]', '', sentence)
-print(special_characters_replaces)
+print('Formatted : ' + special_characters_replaces)
+
+# Note : removes numbers though
+print('\n02. Camel Case to normal string')
+variable_name = 'MyNameIsEshanMEWANTHAHerath'
+print('Original  : ' + variable_name)
+extracted_words = re.findall(r'[A-Z](?:[a-z]+|[A-Z]*(?=[A-Z]|$))', variable_name)
+joined = ' '.join(extracted_words)
+print('Formatted : ' + joined)
